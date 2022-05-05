f4e6a480ca2293a91720996ef48e66470074f6c3
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2022-05-04 21:02:54 -0300

    Criando arquivo c

---
+++ trabalho.c
@@ -0,0 +1,17 @@
+//Trabalho Final - Programação de computadores
+
+/*
+Desenvolvido por:
+    BIANCA BEPPLER DULLIUS
+    CAROLINE SOUZA CAMARGO
+    MARIA JULIA DUARTE LORENZONI
+    YASMIN SOUZA CAMARGO
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Ola Mundo!");
+}
